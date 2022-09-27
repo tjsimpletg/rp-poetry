47bd68445f1a5321ae303fa89a01c8c47ff4daa9
Guoqing ZHANG <tjsimpletg@gmail.com>
2022-09-27 11:08:33 +0200

    feat: add source file

---
+++ rp_poetry/souce_code.py
@@ -0,0 +1,26 @@
+def mult_two(a: int, b: int) -> int:
+    
+    return a*b 
+
+
+
+def checkio(data: list) -> list:
+    res = []
+    for e in data:
+        if data.count(e)>1:
+            res.append(e)
+    return res
+
+
+
+
+
+def flat_list(array):
+    res = []
+    for i in array:
+        if isinstance(i, list):
+            res.extend(flat_list(i))
+        else:
+            res.append(i)
+    return res
+
